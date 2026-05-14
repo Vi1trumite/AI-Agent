1ea9f228e09f3061ae77904c92c72fc25266b8ec
V1ltrumite <viltrumite619@gmail.com>
2026-05-13 22:44:43 -0700

    Imported argparse and added a parser object. Added a message argument and added it to contents while removing the hardcoded response.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 
 load_dotenv()
@@ -11,10 +12,11 @@ if api_key is None:
 
 client =  genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
+parser = argparse.ArgumentParser(description="CLI Chatbot powered by AI")
+parser.add_argument("message", type=str, help="Message you'd like to send to the chatbot.")
+args = parser.parse_args()
+
+response = client.models.generate_content(model="gemini-2.5-flash", contents=args.message)
 if not response.usage_metadata:
     raise RuntimeError("No usage metadata to process...")
 print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
